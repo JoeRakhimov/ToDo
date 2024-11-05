b4574cdf679f076965a467e84fe1c9612e17a109
Joe Rakhimov <joerakhimov@gmail.com>
2024-11-05 12:27:19 +0500

    fix TodoItemView icon for urgent and not important tasks

--- app/src/main/java/com/joerakhimov/todo/TodoItemView.kt
+++ app/src/main/java/com/joerakhimov/todo/TodoItemView.kt
@@ -1,5 +1,7 @@
 package com.joerakhimov.todo
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -13,13 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.joerakhimov.todo.ui.theme.ToDoTheme
+import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.Locale
 
 @Composable
 fun TodoItemView(todoItem: TodoItem) {
@@ -34,39 +37,63 @@ fun TodoItemView(todoItem: TodoItem) {
                     todoItem.importance == Importance.URGENT -> MaterialTheme.colorScheme.error
                     else -> MaterialTheme.colorScheme.onSurface
                 },
+            ).copy(
+                uncheckedBoxColor =
+                when {
+                    todoItem.isCompleted -> MaterialTheme.colorScheme.secondary
+                    todoItem.importance == Importance.URGENT -> MaterialTheme.colorScheme.error.copy(
+                        alpha = 0.16f
+                    )
+                    else -> Color.Transparent
+                }
             ),
             modifier = Modifier.padding(4.dp)
         )
-        if(todoItem.importance == Importance.URGENT) {
-            Text(text = "!!",
-                color = MaterialTheme.colorScheme.error,
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(top = 16.dp, end = 6.dp)
-            )
-        } else if(todoItem.importance == Importance.LOW){
-            Text(text = "↓",
-                color = MaterialTheme.colorScheme.onSurface,
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp,
-                modifier = Modifier.padding(top = 16.dp, end = 6.dp)
-            )
+        if (!todoItem.isCompleted) {
+            if (todoItem.importance == Importance.URGENT) {
+                Image(
+                    painter = painterResource(id = R.drawable.urgent),
+                    contentDescription = "Urgent icon",
+                    modifier = Modifier.padding(top = 20.dp, end = 6.dp)
+                )
+            } else if (todoItem.importance == Importance.LOW) {
+                Image(
+                    painter = painterResource(id = R.drawable.low),
+                    contentDescription = "Not important icon",
+                    modifier = Modifier.padding(top = 20.dp, end = 6.dp)
+                )
+            }
         }
-        Text(
-            text = todoItem.text,
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis,
-            style = MaterialTheme.typography.bodyMedium,
-            textDecoration = if (todoItem.isCompleted) androidx.compose.ui.text.style.TextDecoration.LineThrough else androidx.compose.ui.text.style.TextDecoration.None,
-            color = if(todoItem.isCompleted) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier
+        Column(
+            Modifier
                 .align(Alignment.CenterVertically)
                 .weight(1f)
-                .padding(vertical = 12.dp)
-        )
+                .padding(vertical = 14.dp)
+        ) {
+            Text(
+                text = todoItem.text,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.bodyMedium,
+                textDecoration = if (todoItem.isCompleted) androidx.compose.ui.text.style.TextDecoration.LineThrough else androidx.compose.ui.text.style.TextDecoration.None,
+                color = if (todoItem.isCompleted) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onBackground,
+            )
+            if (!todoItem.isCompleted && todoItem.deadline != null) {
+                Text(
+                    text = SimpleDateFormat(
+                        "dd.MM.yyyy HH:mm",
+                        Locale.getDefault()
+                    ).format(todoItem.deadline),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
         Icon(
             imageVector = Icons.Outlined.Info,
             contentDescription = "Info icon",
+            tint = MaterialTheme.colorScheme.onTertiary,
             modifier = Modifier
                 .padding(vertical = 12.dp, horizontal = 16.dp)
         )
@@ -151,6 +178,23 @@ fun PreviewTodoItemView5() {
         Importance.NORMAL,
         null,
         true,
+        Date(),
+        null
+    )
+    ToDoTheme(dynamicColor = false) {
+        TodoItemView(todoItem = sampleTodoItem2)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTodoItemView6() {
+    val sampleTodoItem2 = TodoItem(
+        "todo_6",
+        "Купить что-то",
+        Importance.NORMAL,
+        Date(),
+        false,
         Date(),
         null
     )
