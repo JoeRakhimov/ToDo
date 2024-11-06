f68599da67bae8b5e3d80c1ec3464bd174e9375e
Joe Rakhimov <joerakhimov@gmail.com>
2024-11-06 15:00:20 +0500

    add task description input and importance selector

--- app/src/main/java/com/joerakhimov/todo/TaskScreen.kt
+++ app/src/main/java/com/joerakhimov/todo/TaskScreen.kt
@@ -1,5 +1,9 @@
 package com.joerakhimov.todo
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,8 +29,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,6 +39,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +49,8 @@ import java.time.LocalDate
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskScreen(onExit: () -> Unit, onSave: () -> Unit) {
-    var importance by remember { mutableStateOf("Normal") }
+    var importance by remember { mutableStateOf("Нет") }
+    var expanded by remember { mutableStateOf(false) }
     var deadlineEnabled by remember { mutableStateOf(false) }
     var deadlineDate by remember { mutableStateOf<LocalDate?>(null) }
     var showImportanceDialog by remember { mutableStateOf(false) }
@@ -50,51 +63,114 @@ fun TaskScreen(onExit: () -> Unit, onSave: () -> Unit) {
                 title = {},
                 navigationIcon = {
                     IconButton(onClick = onExit) {
-                        Icon(Icons.Default.Close, contentDescription = "Exit")
+                        Icon(
+                            Icons.Default.Close,
+                            contentDescription = "Exit",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
                     }
                 },
                 actions = {
-                    Text(stringResource(R.string.save))
+                    Text(
+                        stringResource(R.string.save).uppercase(),
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.labelLarge,
+                        modifier = Modifier.padding(16.dp)
+                    )
                 }
             )
         },
         content = { padding ->
+            val scrollState = rememberScrollState()
             Column(
                 modifier = Modifier
                     .padding(padding)
                     .fillMaxSize()
+                    .verticalScroll(scrollState)
                     .padding(16.dp)
             ) {
-                // Multiline TextField with hint
                 var textFieldValue by remember { mutableStateOf("") }
-                TextField(
-                    value = textFieldValue,
-                    onValueChange = { textFieldValue = it },
-                    label = { Text("Description") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .heightIn(min = 100.dp),
-                    maxLines = 5
-                )
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 4.dp), // Adjust elevation as needed
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextField(
+                        value = textFieldValue,
+                        onValueChange = { textFieldValue = it },
+                        placeholder = { Text(stringResource(R.string.what_to_do)) },
+                        textStyle = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 100.dp),
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = MaterialTheme.colorScheme.surface,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            unfocusedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            unfocusedPlaceholderColor = MaterialTheme.colorScheme.onSurface,
+                            focusedPlaceholderColor = Color.Transparent,
+                        )
+                    )
+                }
 
                 Spacer(modifier = Modifier.height(16.dp))
 
-                // Importance Title with Subtitle
-                Text("Importance", style = MaterialTheme.typography.headlineSmall)
-                Text(importance, style = MaterialTheme.typography.bodyMedium)
-                TextButton(onClick = { showImportanceDialog = true }) {
-                    Text("Change Importance")
-                }
+                Box {
 
-                // Importance Dialog
-                if (showImportanceDialog) {
-                    ImportanceDialog(
-                        onDismiss = { showImportanceDialog = false },
-                        onImportanceSelected = { selectedImportance ->
-                            importance = selectedImportance
-                            showImportanceDialog = false
-                        }
-                    )
+                    Column(
+                        Modifier
+                            .height(72.dp)
+                            .fillMaxWidth()
+                            .clickable { expanded = true },
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            stringResource(R.string.importance),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onPrimary
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            importance,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onTertiary
+                        )
+                    }
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        Modifier.background(MaterialTheme.colorScheme.surface)
+                    ) {
+                        DropdownMenuItem(text = {
+                            Text(
+                                "Нет",
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }, onClick = {
+                            importance = "Нет"
+                            expanded = false
+                        })
+                        DropdownMenuItem(text = {
+                            Text(
+                                "Низкий",
+                                color = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }, onClick = {
+                            importance = "Низкий"
+                            expanded = false
+                        })
+                        DropdownMenuItem(text = {
+                            Row {
+                                Text("!! Высокий", color = MaterialTheme.colorScheme.onError)
+                            }
+                        }, onClick = {
+                            importance = "Высокий"
+                            expanded = false
+                        })
+                    }
                 }
 
                 Spacer(modifier = Modifier.height(16.dp))
@@ -161,7 +237,11 @@ fun ImportanceDialog(onDismiss: () -> Unit, onImportanceSelected: (String) -> Un
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DatePickerDialog(initialDate: LocalDate, onDateSelected: (LocalDate) -> Unit, onDismiss: () -> Unit) {
+fun DatePickerDialog(
+    initialDate: LocalDate,
+    onDateSelected: (LocalDate) -> Unit,
+    onDismiss: () -> Unit
+) {
     val datePicker = remember { LocalDate.now() }
 
     // Assuming you have a date picker available here
